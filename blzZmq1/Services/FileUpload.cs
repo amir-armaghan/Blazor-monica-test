0a6eaa64ba64ae07a49c60cd3d8dca61a37f5898
Amir <amir.armaghan@gmail.com>
2020-04-13 20:13:32 +0200

    first commit

---
+++ blzZmq1/Services/FileUpload.cs
@@ -0,0 +1,25 @@
+﻿using BlazorInputFile;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Threading.Tasks;
+namespace blzZmq1.Services
+{
+    public class FileUpload : IFileUpload
+    {
+        private readonly IWebHostEnvironment _environment;
+        public FileUpload(IWebHostEnvironment env)
+        {
+            _environment = env;
+        }
+        public async Task UploadAsync(IFileListEntry fileEntry)
+        {
+            var path = Path.Combine(_environment.ContentRootPath, "Upload", fileEntry.Name);
+            var ms = new MemoryStream();
+            await fileEntry.Data.CopyToAsync(ms);
+            using (FileStream file = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                ms.WriteTo(file);
+            }
+        }
+    }
+}
---
+++ blzZmq1/Services/IFileUpload.cs
@@ -0,0 +1,9 @@
+﻿using BlazorInputFile;
+using System.Threading.Tasks;
+namespace blzZmq1.Services
+{
+    public interface IFileUpload
+    {
+        Task UploadAsync(IFileListEntry file);
+    }
+}
