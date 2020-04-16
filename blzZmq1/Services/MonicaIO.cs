ff9760b5d4dfbfbb70b9e417810e525a42b87224
Amir Armaghan <63227708+armaghan-work@users.noreply.github.com>
2020-04-16 17:03:12 +0200

    First step to read json files in a relative path

---
+++ blzZmq1/Services/MonicaIO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace blzZmq1.Services
+{
+    public class MonicaIO
+    {
+
+        dynamic dyn;
+        string json_sim = @"Data/sim.json";
+        string json_site = @"Data/site.json";
+        string json_crop = @"Data/crop.json";
+        string csv_climate = @"Data/climate.csv";
+        FileStream inputStream = new FileStream("sim.json", FileMode.Open);
+
+        //protected override void ReadJsons()
+       // {
+         //   dyn = JsonConvert.DeserializeObject(json_sim);
+            // ... read the rest
+       // }
+    }
+    
+
+}
