2ae54f2f0193b2741dc0ed83d057d4b5c0d7f21a
Amir Armaghan <63227708+armaghan-work@users.noreply.github.com>
2020-04-27 17:26:15 +0200

    covert Json2Objects2String 4 HighchartJS

---
+++ blzZmq1/Services/GetInfoFromJson.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
+
+namespace blzZmq1.Services
+{
+    public class GetInfoFromJson
+    {
+
+        // only get the dates
+        public static string GetDate()
+        {
+        string chartString = "";
+        var chartData = JsonToChart.ConvertJson();
+        
+        foreach (var x in chartData.Data[0].OutputIds[0].Result)
+                if (x != null)
+                {
+                    //Console.Write("'" + Convert.ToString(x) + "' , ");
+                    chartString = chartString + "'" + Convert.ToString(x) + "' , ";
+
+
+                }
+        return chartString;
+
+        }
+
+        public static string GetPrecip() //only for test
+        {
+            string chartString = "";
+            var chartData = JsonToChart.ConvertJson();
+
+            //string chartString = string.Join(",", chartData.Data[0].OutputIds[8].Result as List<object[]>.ToList<float[]>());
+            //var y = chartData.Data[0].OutputIds[8].Result[550]; // 8.5
+
+             foreach (var x in chartData.Data[0].OutputIds[8].Result)
+                 if (x != null)
+                 {
+                    chartString = chartString + Convert.ToString(x).Replace(",", ".") + ", ";
+                 }
+            
+            return chartString;
+
+        }
+    }
+}
---
+++ blzZmq1/Services/JsonToChart.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
+namespace blzZmq1.Services
+{
+    public class JsonToChart
+        {    
+           
+
+    public static ChartData ConvertJson()
+        {
+            string schemaJson = File.ReadAllText(@"Data/jsonSchema.schema.json");
+            JsonSchema schema = JsonSchema.Parse(schemaJson);
+            var chartData = new ChartData();
+
+            using (StreamReader file = File.OpenText(@"Data/fromServer.json"))
+            {
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    JObject jObject = (JObject)JToken.ReadFrom(reader);
+
+                    // Validating the JSON file structure by comparing it with the schema file
+                    if (jObject.IsValid(schema))
+                    {
+                        Console.WriteLine("Jaon file is not valid.");
+                    }
+                    else
+                    {
+                        chartData.CustomId = jObject[ConvertToPascalCase(nameof(ChartData.CustomId))];
+                        chartData.Errors = jObject[ConvertToPascalCase(nameof(ChartData.Errors))] as IList<object>;
+                        chartData.Type = (string)jObject[ConvertToPascalCase(nameof(ChartData.Type))];
+                        chartData.Warnings = jObject[ConvertToPascalCase(nameof(ChartData.Warnings))] as IList<object>;
+
+                        foreach (var mData in jObject["data"])
+                        {
+
+                            if (chartData.Data == null)
+                                chartData.Data = new List<MainData>();
+
+                            var mainData = new MainData();
+
+                            mainData.OrigSpec = (string)mData[ConvertToPascalCase(nameof(mainData.OrigSpec))];
+
+                            int resultIndex = 0;
+
+                            foreach (var outId in mData[ConvertToPascalCase(nameof(mainData.OutputIds))])
+                            {
+                                if (mainData.OutputIds == null)
+                                    mainData.OutputIds = new List<OutputId>();
+
+                                var outputId = new OutputId();
+
+                                outputId.Id = (int)outId[ConvertToPascalCase(nameof(outputId.Id))];
+                                outputId.JsonInput = (string)outId[ConvertToPascalCase(nameof(outputId.JsonInput))];
+                                outputId.LayerAggOp = (int)outId[ConvertToPascalCase(nameof(outputId.LayerAggOp))];
+                                outputId.Name = (string)outId[ConvertToPascalCase(nameof(outputId.Name))];
+                                outputId.Organ = (int)outId[ConvertToPascalCase(nameof(outputId.Organ))];
+                                outputId.TimeAggOp = (int)outId[ConvertToPascalCase(nameof(outputId.TimeAggOp))];
+                                outputId.ToLayer = (int)outId[ConvertToPascalCase(nameof(outputId.ToLayer))];
+                                outputId.Type = (string)outId[ConvertToPascalCase(nameof(outputId.Type))];
+                                outputId.Unit = (string)outId[ConvertToPascalCase(nameof(outputId.Unit))];
+
+                                outputId.Result = ((JArray)mData[ConvertToPascalCase("results")][resultIndex++]).ToList<object>();
+
+                                mainData.OutputIds.Add(outputId);
+                            }
+
+                            chartData.Data.Add(mainData);
+                        }
+                    }
+                }
+            }
+
+            return chartData;
+        }
+
+        static string ConvertToPascalCase(string str)
+        {
+            char firstLetter = char.ToLower(str[0]);
+            return firstLetter + str.Substring(1, str.Length - 1);
+        }
+
+    }
+
+ 
+
+    public class ChartData
+    {
+        public object CustomId { get; set; }
+        public IList<MainData> Data { get; set; }
+        public IList<object> Errors { get; set; }
+        public string Type { get; set; }
+        public IList<object> Warnings { get; set; }
+    }
+
+    public class MainData
+    {
+        public string OrigSpec { get; set; }
+        public IList<OutputId> OutputIds { get; set; }
+    }
+
+    public class OutputId
+    {
+        public string DisplayName { get; set; }
+        public int FromLayer { get; set; }
+        public int Id { get; set; }
+        public string JsonInput { get; set; }
+        public int LayerAggOp { get; set; }
+        public string Name { get; set; }
+        public int Organ { get; set; }
+        public int TimeAggOp { get; set; }
+        public int ToLayer { get; set; }
+        public string Type { get; set; }
+        public string Unit { get; set; }
+        public IList<object> Result { get; set; }
+
+    }
+
+}
